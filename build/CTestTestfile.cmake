ac2bda7f70558fb8ef829d333f08492a9b407030
MarissaCampa <marissag.campa@gmail.com>
2022-06-06 16:23:42 -0400

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ReAuto/Documents/cpp_projects/Accounts
-# Build directory: C:/Users/ReAuto/Documents/cpp_projects/Accounts/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
